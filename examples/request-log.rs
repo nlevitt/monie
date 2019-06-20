9ace345111338a539014e1ca080786f75c2c3151
Noah Levitt <nlevitt@archive.org>
2019-06-20 14:24:59 -0700

    new example request-log and remove old example

--- examples/request-log.rs
+++ examples/request-log.rs
@@ -1,12 +1,11 @@
-//! A simple mitm proxy example using monie. Adds via header to each request
-//! and response. Logs each request.
+//! A simple mitm proxy example using monie. Logs each request with request
+//! method, response status code, size and url.
 
 #![deny(warnings)]
 
 use std::sync::Mutex;
 
 use futures::future::Future;
-use http::header::{HeaderMap, HeaderValue};
 use http::method::Method;
 use http::status::StatusCode;
 use http::uri::Uri;
@@ -15,41 +14,16 @@ use hyper::{Body, Chunk, Request, Response, Server};
 use monie::{Mitm, MitmProxyService};
 
 #[derive(Debug)]
-struct SimpleMitm {
+struct LogsRequest {
     uri: Uri,
     method: Mutex<Option<Method>>,
     status: Mutex<Option<StatusCode>>,
     length: Mutex<usize>,
 }
 
-fn numeric_bytes(version: http::Version) -> &'static [u8] {
-    match version {
-        http::Version::HTTP_09 => b"0.9",
-        http::Version::HTTP_10 => b"1.0",
-        http::Version::HTTP_11 => b"1.1",
-        http::Version::HTTP_2  => b"2.0",
-    }
-}
-
-fn set_via(headers: &mut HeaderMap, version: &http::Version) {
-    let mut buf = match headers.remove("via") {
-        Some(existing_via) => {
-            let mut buf = existing_via.as_bytes().to_vec();
-            buf.extend_from_slice(b", ");
-            buf
-        },
-        None => {
-            Vec::new()
-        },
-    };
-    buf.extend_from_slice(numeric_bytes(*version));
-    buf.extend_from_slice(b" monie-example");
-    headers.insert("via", HeaderValue::from_bytes(&buf).unwrap());
-}
-
-impl Mitm for SimpleMitm {
-    fn new(uri: http::uri::Uri) -> SimpleMitm {
-        SimpleMitm {
+impl Mitm for LogsRequest {
+    fn new(uri: http::uri::Uri) -> LogsRequest {
+        LogsRequest {
             uri: uri,
             method: Mutex::new(None),
             status: Mutex::new(None),
@@ -58,25 +32,19 @@ impl Mitm for SimpleMitm {
     }
 
     fn request_headers(&self, req: Request<Body>) -> Request<Body> {
-        let (mut parts, body) = req.into_parts();
+        let (parts, body) = req.into_parts();
 
-        // take note of method and uri for logging
+        // take note of method for logging
         *self.method.lock().unwrap() = Some(parts.method.clone());
-
-        // set via header
-        set_via(&mut parts.headers, &parts.version);
 
         Request::from_parts(parts, body)
     }
 
     fn response_headers(&self, res: Response<Body>) -> Response<Body> {
-        let (mut parts, body) = res.into_parts();
+        let (parts, body) = res.into_parts();
 
         // take note of status for logging
         *self.status.lock().unwrap() = Some(parts.status.clone());
-
-        // set via header
-        set_via(&mut parts.headers, &parts.version);
 
         Response::from_parts(parts, body)
     }
@@ -86,12 +54,13 @@ impl Mitm for SimpleMitm {
     }
 
     fn response_body_chunk(&self, chunk: Chunk) -> Chunk {
+        // add to length for logging
         *self.length.lock().unwrap() += chunk.len();
         chunk
     }
 }
 
-impl Drop for SimpleMitm {
+impl Drop for LogsRequest {
     fn drop(&mut self) {
         println!("{:9} {} {} {}",
                  self.length.lock().unwrap(),
@@ -104,7 +73,7 @@ impl Drop for SimpleMitm {
 fn main() {
     pretty_env_logger::init_timed();
     let addr = ([127, 0, 0, 1], 8000).into();
-    let svc = MitmProxyService::<SimpleMitm>::new();
+    let svc = MitmProxyService::<LogsRequest>::new();
     let server = Server::bind(&addr)
         .serve(svc)
         .map_err(|e| eprintln!("server error: {}", e));
