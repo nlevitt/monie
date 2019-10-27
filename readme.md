d772f58391aecfe63f728bad4d53cfa552f95118
Shaun Verch <shaun@shaunverch.com>
2019-10-26 23:59:35 -0400

    Add env variables for custom certificate and key
    MONIE_KEY_FILE and MONIE_CERT_FILE can be used to control what
    certificate is served up by this server.
    
    Taken mostly from
    https://github.com/ctz/hyper-rustls/blob/master/examples/server.rs.

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ mitm http/https proxy library in rust
 Should be fast. Asynchronous. Uses https://github.com/hyperium/hyper.
 
 <img src="https://img.discogs.com/QphGS1igloXHNKiBiPe8936I1hI=/fit-in/600x535/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-244973-1219689437.jpeg.jpg" alt="monie in the middle" width="279" height="249">
+
+Set `MONIE_CERT_FILE` and `MONIE_KEY_FILE` environment variables if you want to
+use a custom certificate/private key.
--- src/certauth.rs
+++ src/certauth.rs
@@ -8,6 +8,10 @@
 #![deny(missing_debug_implementations)]
 
 use std::sync::{Arc, Mutex};
+use std::env;
+
+use rustls::internal::pemfile;
+use std::{fs, io};
 
 use http::uri::Authority;
 use lru_cache::LruCache;
@@ -16,6 +20,9 @@ use openssl::hash::MessageDigest;
 use openssl::pkey::PKey;
 use openssl::rsa::Rsa;
 use openssl::x509::{X509Builder, X509NameBuilder};
+
+const MONIE_KEY_FILE: &str = "MONIE_KEY_FILE";
+const MONIE_CERT_FILE: &str = "MONIE_CERT_FILE";
 
 lazy_static! {
     static ref TLS_CONFIG_CACHE: Mutex<LruCache<String, Arc<rustls::ServerConfig>>> =
@@ -61,6 +68,56 @@ pub fn gen_key_cert(
     (key, cert)
 }
 
+fn error(err: String) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, err)
+}
+
+// Load public certificate from file.
+fn load_certs(filename: &str) -> io::Result<Vec<rustls::Certificate>> {
+    // Open certificate file.
+    let certfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(certfile);
+
+    // Load and return certificate.
+    pemfile::certs(&mut reader).map_err(|_| error("failed to load certificate".into()))
+}
+
+// Load private key from file.
+fn load_private_key(filename: &str) -> io::Result<rustls::PrivateKey> {
+    // Open keyfile.
+    let keyfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(keyfile);
+
+    // Load and return a single private key.
+    let keys = pemfile::rsa_private_keys(&mut reader)
+        .map_err(|_| error("failed to load private key".into()))?;
+    if keys.len() != 1 {
+        return Err(error("expected a single private key".into()));
+    }
+    Ok(keys[0].clone())
+}
+
+/// Get a key and self-signed cert for the provided authority, checking to see if a static cert has
+/// been provided.
+pub fn get_key_certs(
+    authority: &Authority,
+) -> (rustls::PrivateKey, Vec<rustls::Certificate>) {
+    if env::var_os(MONIE_KEY_FILE).is_some() &&
+        env::var_os(MONIE_CERT_FILE).is_some() {
+        let keyfile = env::var(MONIE_KEY_FILE).unwrap();
+        let certfile = env::var(MONIE_CERT_FILE).unwrap();
+        let key = load_private_key(&keyfile).expect("Failed loading key");
+        let certs = load_certs(&certfile).expect("Failed loading certs");
+        (key, certs)
+    } else {
+        let (key, cert) = gen_key_cert(&authority);
+        let certs = vec![cert; 1];
+        (key, certs)
+    }
+}
+
 /// Either load an existing TLS server configuration from cache or build a new
 /// one (and cache it) for the provided authority.
 pub fn tls_config(authority: &Authority) -> Arc<rustls::ServerConfig> {
@@ -70,8 +127,7 @@ pub fn tls_config(authority: &Authority) -> Arc<rustls::ServerConfig> {
         .contains_key(authority.host())
     {
         let tls_cfg: Arc<rustls::ServerConfig> = {
-            let (key, cert) = gen_key_cert(&authority);
-            let certs = vec![cert; 1];
+            let (key, certs) = get_key_certs(&authority);
             let mut result =
                 rustls::ServerConfig::new(rustls::NoClientAuth::new());
             result
